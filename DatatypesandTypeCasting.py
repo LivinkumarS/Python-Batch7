c873ad1cf9c62dd2bb4668b8f4a35136ea8345bf
LivinkumarS <001liv100@ggmail.com>
2025-08-08 14:39:56 +0530

    Datatypes and TypeCasting

---
+++ DatatypesandTypeCasting.py
@@ -0,0 +1,54 @@
+# a=23
+# b=1.2
+
+# str1= 'I would love to code'
+# str2= "I would love to workout"
+# str3=str1+". "+str2
+
+# print(str3)
+
+
+# name="Surya"
+# latest_movie="Retro"
+
+# print("His name is "+name+". His latest movie is "+latest_movie+'.')
+
+# print(f'His name is {name}. His latest movie is {latest_movie}.')
+
+# His name is {}. His latest movie is {}.
+
+
+# isPresent=True
+# isPresent=False
+# print(isPresent)
+
+
+# a=True
+# print(type(a))
+
+# a=100
+# print(type(a))
+
+# b=str(a)
+# print(b)
+# print(type(b))
+
+# a=10
+# b=80
+
+# str1="Hello"
+# str2="Hi"
+
+# print(str1+a)
+
+# name="Vijay"
+# age=52
+
+# print("His name is "+ name+". His age is "+str(age)+".")
+# print(f"His name is {name}. His age is {age}")
+
+# user n1= anything
+# print--> anything+10
+
+#n1=  #'100'
+print(int(input("Enter Number: "))+10)
