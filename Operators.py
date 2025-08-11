a8a59a62689681a101780e8593bdd18d62c87a92
LivinkumarS <001liv100@ggmail.com>
2025-08-11 13:36:26 +0530

    Operators

---
+++ Operators.py
@@ -0,0 +1,64 @@
+# Arithmetic
+
+# a=10
+# b=20
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a/b)
+# print(a//b) # 10//20 -->0
+
+# print(a%b)
+# print(100%3)
+# print(3**3)
+
+
+# print(2+3-6*8/2) #-19
+
+
+# Comparison
+# a=100
+# b=101
+
+# print(a==100)
+# print(a!=b-1)
+# print(a>b)
+# print(a<100)
+# print(a>=99)
+# print(a<=b)
+
+# Logical operator 
+
+# a=True
+# b=True
+
+# print( a and b )
+# print( a or b )
+# print(not not b)
+
+# print((a and (not b)) and (not a or b and not (a or b and not (a or b and a))))
+
+
+
+# Assignment Operator 
+
+# b=9
+# a=b
+# a+=8 #10+8
+# a+=10
+# a-=10 # 10-10
+# a+=18
+# a*=987
+# a/=10
+
+# a**=2
+
+a=10
+a+=-10
+a-=-10
+a**=2
+a/=100
+
+
+# print(a)
+print(a)
