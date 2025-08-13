a4c5c4e00db50b3ee9a3fb449f003ffc665e0484
LivinkumarS <001liv100@ggmail.com>
2025-08-13 12:58:57 +0530

    Loops

---
+++ Loops.py
@@ -0,0 +1,54 @@
+# a=0
+
+# while a<10: 
+#     print(a)
+#     a-=1
+
+# a=0,-1,-2,-3,-4
+# iter=1,2,3,4,5,
+
+# a=0
+
+# while a<5:
+#     print("Step1")
+#     print("Step2")
+#     print("Step3")
+#     a+=1
+
+# str1="I would like to go to movies!"
+
+# for char in str1:
+#     print(char)
+
+# for i in range(10,20,3): # 10,13,16,19
+#     print(i)
+
+
+# while True:
+#     secret=10
+#     a=int(input("Enter The number: "))
+
+#     if(a==secret):
+#         print("You won!")
+#         break
+#     else:
+#         print("Try Again!")
+        
+# for i in range(0,5): # 0,1,2,3,4
+#     if(i==3):
+#         continue
+#     print(i)
+#     print("-----------")
+
+# for i in range(0,51,2):
+#     print(i)
+
+# print(1)
+# print(2)
+# print(4)
+# print(7)
+# print(8)
+# print(11)
+# print(13)
+# print(14)
+# print("Buzz")
