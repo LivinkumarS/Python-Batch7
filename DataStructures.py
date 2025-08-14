48bf99694ec5de52d283a1a79d13b0f1af40081e
LivinkumarS <001liv100@ggmail.com>
2025-08-14 11:31:37 +0530

    DataStructures - List

---
+++ DataStructures.py
@@ -0,0 +1,40 @@
+# arr=[
+#     1,
+#     2,
+#     3,
+#     4,
+#     [
+#         "six",
+#         "seven",
+#         "eight",
+#         [
+#             "nine",
+#             "ten",
+#             [
+#                 11,
+#                 12,
+#                 13
+#             ]
+#         ]
+#     ]
+# ]
+
+# print(arr[-1][3][1][-1])
+
+arr=[32,3,34,4,34,54,76,46,4,5,56,46,46]
+
+# arr[4]=100
+
+# arr.append(1000)
+# arr.append(2000)
+# arr.insert(1,-1)
+# arr.remove(4)
+# arr.pop()
+
+arr.sort()
+arr.reverse()
+
+
+
+
+print(arr)
