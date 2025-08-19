7e4b656321ef7235dd0411c759730e8228ff1424
LivinkumarS <001liv100@ggmail.com>
2025-08-19 11:21:08 +0530

    String and it's in built methods

---
+++ String.py
@@ -0,0 +1,37 @@
+str1="Hello Guys!, I am Livinkumar from tamlinadu!"
+# ans=str1[:7] 
+# str1[0]='A'
+# print(str1)
+# ans=str1.strip()
+# ans=str1.upper()
+# ans=str1.lower()
+
+# ans=str1.startswith('Hello  W')
+# ans=str1.endswith('dl!')
+# ans=str1.replace('Hello','Hi')
+
+# ans=str1.find('War')
+# ans=str1.count('s')
+# ans=str1.split()
+
+# lis1=['Hello', 'Guys!,', 'I', 'am', 'Livinkumar', 'from', 'tamlinadu!']
+# ans="-".join(lis1)
+
+# name="Livin"
+# age=22
+
+# ans=f"Hi from {name}. I am {age} years old!"
+# ans= "Hi from "+name+". I am "+str(age)+" years old!"
+
+# ans="Hello Guys!.\nI am from karur."
+ans="I'm Livinkumar... My fav food is \"sea food\""
+
+# print(str1)
+print(ans)
+
+
+# lis1=[1,2,3,4,5]
+# lis1[3]=100
+# print(lis1)
+
+# print(len(str1))
