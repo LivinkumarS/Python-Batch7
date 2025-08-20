54eec2a42ab97a36731eef91e1112f554cc5c976
LivinkumarS <001liv100@ggmail.com>
2025-08-20 11:40:52 +0530

    OOPS intro

---
+++ OOPS.py
@@ -0,0 +1,13 @@
+class Thalapathy:
+    def __init__(self):
+        pass
+    
+    def sayDi(self):
+        print("I am waiting!")
+    
+    def partyName(self):
+        print("TVK")
+        
+a=Thalapathy()
+
+a.partyName()
